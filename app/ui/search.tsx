bb840ae15e6e236f66dcea9f1dd0884fcc525f6f
atsushi <london.rightside@gmail.com>
2024-07-06 16:47:09 +0900

    第11章検索とページネーションの追加

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,55 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
-  );
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // 入力を停止してから300ms後に検索を実行する（デバウンス）
+  // デバウンスされた関数をコンポーネントのトップレベルで定義
+  const debouncedHandleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    console.log(term);
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  /**
+   * 検索文字入力エリアハンドラ。
+   * 入力された検索文字をリアルタイムにURLに反映させる。
+   * @param term 検索文字
+   */
+  function handleSearch(term: string) {
+    debouncedHandleSearch(term);
+  }
+
+return (
+  <div className="relative flex flex-1 flex-shrink-0">
+    <label htmlFor="search" className="sr-only">
+      Search
+    </label>
+    <input
+      className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+      placeholder={placeholder}
+      onChange={(e) => {
+        handleSearch(e.target.value);
+      }}
+      defaultValue={searchParams.get('query')?.toString()}
+    />
+    <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+  </div>
+);
 }
